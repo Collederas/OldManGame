9e63a0d3e2e098b35c08170faf04c990242e2073
Fabrizio Scandola <fabrizio.scandola@elements.nl>
2020-12-13 17:58:47 +0100

    GP - Basic health/die mechanic, with UI

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// Thanks Brakeys! --> https://www.youtube.com/watch?v=BLfNP4Sc_iA&ab_channel=Brackeys
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
