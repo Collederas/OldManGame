63d197af31a5c9928d04d4d98561639e628b6bcd
Fabrizio Scandola <fabrizio.scandola@elements.nl>
2020-12-27 12:22:13 +0100

    Add basic Dialogue system
    It comes with reorganising the object in Scene 1 hiereachy
    and removing Crossfade canvas from the LevelLoader (it still
    can be found as a dependency)

---
+++ Assets/Scripts/Gameplay/DialogueTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public DialogueManager dialogueManager;
+    private GameManager _gameManager;
+
+    private void Awake()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
+
+        if (dialogueManager == null)
+        {
+            Debug.LogWarning("DialogueManager dependency is empty. Trying to locate a DialogueManager" +
+                             " object in current Scene");
+            dialogueManager = FindObjectOfType<DialogueManager>();
+            if (dialogueManager == null)
+            {
+                Debug.LogWarning("Can't locate a DialogueManager in current scene");
+                return;
+            }
+        }
+        dialogueManager.DialogueEnded += OnDialogueEnded;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _gameManager.GetPlayer().DeactivateInput();
+        dialogueManager.StartDialogue(dialogue);
+    }
+
+    private void OnDialogueEnded()
+    {
+        _gameManager.GetPlayer().ActivateInput();
+        Destroy(gameObject);
+    }
+}
